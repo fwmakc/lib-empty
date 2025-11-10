1119427dd5d87748948ecdf679483afb5a1ef75f
fwmakc <fwmakcpro@yandex.ru>
2025-11-10 16:19:25 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { emptyArray } from './helpers/empty_array.helper';
 import { emptyObject } from './helpers/empty_object.helper';
-import { emptyValue } from './helpers/empty_value.helper';
+import { isEmpty } from './helpers/is_empty.helper';
+import { notEmpty } from './helpers/not_empty.helper';
 import { notEmptyArray } from './helpers/not_empty_array.helper';
 import { notEmptyObject } from './helpers/not_empty_object.helper';
-import { notEmptyValue } from './helpers/not_empty_value.helper';
 
 export {
   emptyArray,
   emptyObject,
-  emptyValue,
+  isEmpty,
   notEmptyArray,
   notEmptyObject,
-  notEmptyValue,
+  notEmpty,
 };
